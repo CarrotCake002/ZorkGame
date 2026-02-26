97c4a38012323e934fdd6a50d427739bdcad03ad
Pol Siles <puliki82002@gmail.com>
2026-02-26 22:45:26 +0100

    Added a ton of new commands: look to look around drop to drop items from your inventory to the world take to take items from the world and add them to your inventory inventory/i to check inventory items

--- ZorkGame/main.cpp
+++ ZorkGame/main.cpp
@@ -1,24 +1,23 @@
-#include "Game.h"
 #include "World.h"
 
 int main() {
     int status = true;
     World world;
 
-    Player* player = new Player("Player", "the player character.", 10, 2, EntityType::Player);
-    player->addItem(new Entity("Stick", "a stick to attack enemies with."));
+    Player* player = new Player("Player", "the player character", 10, 2, EntityType::Player);
+    player->addItem(new Entity("Stick", "a stick to attack enemies with"));
 
-    Creature* creature = new Creature("NPC", "a small goofy ennemy.", 10, 1);
-    creature->addItem(new Entity("Rock", "a rock that deal damage with every hit."));
+    Creature* creature = new Creature("NPC", "a small goofy ennemy", 10, 1);
+    creature->addItem(new Entity("Rock", "a rock that deal damage with every hit"));
 
     world.addEntity(player);
 	world.addEntity(creature);
 
-    Entity* bag = new Entity("Smal bag", "a small bag that can contain a few items.");
+    Entity* bag = new Entity("Bag", "a small bag that can contain a few items");
 
-    Entity* hat = new Entity("Cool hat", "a cool hat for daring fashion enjoyers.");
-    Entity* shirt = new Entity("Nice shirt", "an elegant shirt for formal events.");
-    Entity* pants = new Entity("Pair of pants", "nice pants that would pair perfectly with a shirt.");
+    Entity* hat = new Entity("Hat", "a cool hat for daring fashion enjoyers");
+    Entity* shirt = new Entity("Shirt", "an elegant shirt for formal events");
+    Entity* pants = new Entity("Pants", "nice pants that would pair perfectly with a shirt");
 
     bag->addItem(hat);
     bag->addItem(shirt);
@@ -26,11 +25,10 @@ int main() {
 
     world.addEntity(bag);
 
-    while (status) {
-        if (getInput() == 1) {
+    while (world.isRunning()) {
+        if (world.getInput() == 1) {
             break;
         }
-		world.update();
     }
     return 0;
 }
