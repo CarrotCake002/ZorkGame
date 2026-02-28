333dc654f36121230cec9bd4013bbfeff1fbdc3c
Pol Siles <puliki82002@gmail.com>
2026-02-28 20:49:24 +0100

    added the ability to walk between rooms. You can go back and forth, and nonexistant exits cannot be crossed, obviously...

--- ZorkGame/Room.cpp
+++ ZorkGame/Room.cpp
@@ -3,3 +3,17 @@
 Room::Room(std::string name, std::string description) : Entity(name, description, EntityType::Room) {
 
 }
+
+Exit* Room::getExit(Direction dir) const {
+	Exit* exit = nullptr;
+
+	for (auto& entity : contains) {
+		if (entity->getType() == EntityType::Exit) {
+			exit = static_cast<Exit*>(entity);
+			if (exit->getDirection() == dir) {
+				return exit;
+			}
+		}
+	}
+	return nullptr;
+}
